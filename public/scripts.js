e3a4f9c7d37601b828159a2d14aaa6470c716c9b
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-04-27 21:35:01 +0530

    updating DOM room info

--- public/scripts.js
+++ public/scripts.js
@@ -19,6 +19,28 @@ socket.on("nsList", (nsData) => {
       console.log(`${nsEndpoint} I should go to now.`);
     });
   });
+
+  const nsSocket = io("http://localhost:5000/wiki");
+  nsSocket.on("nsRoomLoad", (nsRooms) => {
+    let roomList = document.querySelector(".room-list");
+    roomList.innerHTML = "";
+    nsRooms.forEach((room) => {
+      let glyph;
+      if (room.privateRoom) glyph = "lock";
+      else glyph = "globe";
+      roomList.innerHTML += `
+      <li class="room">
+      <span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}
+    </li>
+      `;
+    });
+    let roomNodes = document.getElementsByClassName("room");
+    Array.from(roomNodes).forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        console.log("Some one clicked on", e.target.innerText);
+      });
+    });
+  });
 });
 
 socket.on("messageFromServer", (dataFromServer) => {
